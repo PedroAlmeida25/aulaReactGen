cd25455f2112ef1c1c92a52b9ee714df70f7be2e
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-05-16 12:14:48 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Tarefa from './pages/tarefa/Tarefa'
+// import Contador from './pages/contador/Contador'
+// import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <>
+        {/* <Home 
+          titulo='Componente Home'
+          texto='Este é um Componente que recepe props.'
+        />
+
+        <Contador /> */
+        <Tarefa />
+        }
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,29 @@
+﻿import { useEffect, useState } from "react"
+
+function Tarefa() {
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect(() => {
+    if(completed){
+      setTarefa('Parabéns!, Tarefa Concluída!')
+    }else{
+      setTarefa('')
+    }
+  }, [completed])
+
+
+  return (
+    <>
+    <h2>Componente Tarefa</h2>
+
+    <h3>{tarefa}</h3>
+
+    <p>Conclua a tarefa</p>
+
+    <button onClick={() => setCompleted(true)}>Concluir a tarefa</button>
+    </>
+  )
+}
+
+export default Tarefa
